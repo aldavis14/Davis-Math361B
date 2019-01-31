8d80480aa659a9d8e933fdc02844f4d65a42222f
Allison Davis <>
2019-01-30 20:34:00 -0500

    Problem I.2 - final commit

---
+++ IntroToProgramming/Calculator_Davis.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 30 20:07:10 2019
+
+@author: allisondavis
+"""
+
+#input variables
+
+x=1
+y=2
+z=3
+
+calculator=[x+y,(y*z)+(3*x),(x+y)**2,(2*((y*z)+(3*x))-(0.5*x))/(x+y),7%3]
+
+print(calculator)
+
