10f46d8d51e95e49c19e0bfabce60006ceee3d8e
Allison Davis <>
2019-02-06 10:52:08 -0500

    Correct Calculator File

--- IntroToProgramming/Calculator_Davis.py
+++ IntroToProgramming/Calculator_Davis.py
@@ -8,11 +8,19 @@ Created on Wed Jan 30 20:07:10 2019
 
 #input variables
 
-x=1
-y=2
-z=3
+x=
+y=
+z=
+#%%
+#creating the list
 
 calculator=[x+y,(y*z)+(3*x),(x+y)**2,(2*((y*z)+(3*x))-(0.5*x))/(x+y),7%3]
-
 print(calculator)
 
+calculator[2]=calculator[2]+3
+print(calculator)
+
+calculator[-1]=calculator[-1]*(3/4)
+print(calculator)
+
+print('The sum of the list is',sum(calculator))
