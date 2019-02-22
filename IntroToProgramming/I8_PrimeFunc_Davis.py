308a44c6179f63d13b77876f5b44a7290fea305c
Allison Davis <>
2019-02-22 12:23:35 -0500

    primality function

--- IntroToProgramming/I8_PrimeFunc_Davis.py
+++ IntroToProgramming/I8_PrimeFunc_Davis.py
@@ -11,22 +11,22 @@ import numpy as np
 #define the function
 def prime_check(x):
     if x==1:
-        return 'false'
+        return False
     for i in range(2, math.floor(np.sqrt(x))+1):
         if x%i==0:
-            return ('false')
-    return('true')
+            return (False)
+    return(True)
         
 
 #%% examples
-prime_check(79)
+prime_check(55)
 
 #%% determine the nth prime number
 n=5
 
 primes=[]
 for ii in range(1,1000):
-    if prime_check(ii)=='true':
+    if prime_check(ii)==True:
         primes.append(ii)
         
 # Print the nth prime 
