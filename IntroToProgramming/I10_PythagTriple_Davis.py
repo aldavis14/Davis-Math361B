ba5e377e8758b0bcf048ef7ee784a88b4d9771a0
Allison Davis <>
2019-02-26 20:45:25 -0500

    Beginning I.10

---
+++ IntroToProgramming/I10_PythagTriple_Davis.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 20:00:25 2019
+
+@author: allisondavis
+"""
+#import libraries
+import numpy as np
+
+#%% Create the triples
+triples = np.zeros([0,4])
+for i in range(1,500):
+    for j in range(1,500):
+        for k in range(1,500):
+            if i**2+j**2 == k**2 and i<j<k:
+                triples = np.vstack( [triples, np.array([i,j,k,i+j+k])])
+
+#%% Find the triple for which a+b+c=1026
+print(triples[np.where(triples[:,3]==1026)],'is the triple that we wanted to find.')
